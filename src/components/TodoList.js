4f79e8b8f3e42a69e166a2236807321667f45aeb
TheAlmightyG <Dylan.M.Waters@hotmail.com>
2024-12-01 10:02:04 -0500

    Added Complete Option

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,36 +1,57 @@
 import React, { useState } from 'react';
+
 function TodoList() {
     const [todos, setTodos] = useState([]);
     const [inputValue, setInputValue] = useState('');
+
     function handleInputChange(event) {
         setInputValue(event.target.value);
-       }
-    function handleInputChange(event) {
-    setInputValue(event.target.value);
     }
+
     function handleSubmit() {
         if (inputValue.trim()) {
-        setTodos([...todos, inputValue.trim()]);
-        setInputValue('');
+            setTodos([...todos, { text: inputValue.trim(), completed: false }]);
+            setInputValue('');
         }
-       }
-       function handleDelete(index) {
+    }
+
+    function handleDelete(index) {
         setTodos(todos.filter((_, i) => i !== index));
-       }
-       
-return (
-<div>
-<input type="text" value={inputValue} onChange={handleInputChange} />
-<button onClick={handleSubmit}>Add To-do Item</button>
-<ul>
- {todos.map((todo, index) => (
- <li key={index}>
- {todo}
- <button onClick={() => handleDelete(index)}>Remove To-do Item</button>
- </li>
- ))}
-</ul>
-</div>
-);
+    }
+
+    function toggleCompletion(index) {
+        setTodos(
+            todos.map((todo, i) =>
+                i === index ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    }
+
+    return (
+        <div>
+            <input type="text" value={inputValue} onChange={handleInputChange} />
+            <button onClick={handleSubmit}>Add To-do Item</button>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li
+                        key={index}
+                        style={{
+                            textDecoration: todo.completed ? 'line-through' : 'none',
+                            color: todo.completed ? 'gray' : 'black',
+                        }}
+                    >
+                        {todo.text}
+                        <button onClick={() => toggleCompletion(index)}>
+                            {todo.completed ? 'Undo' : 'Complete'}
+                        </button>
+                        <button onClick={() => handleDelete(index)}>
+                            Remove To-do Item
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
+
 export default TodoList;
